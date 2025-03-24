5f308d47f67abf6b1d807002b1b416b3e1544d01
nevertoomuch <_email@example.com>
2025-03-24 18:04:51 +0700

    Добавил второй комментрай

--- sort.c
+++ sort.c
@@ -4,7 +4,7 @@ void bubbleSort(int arr[], int n) {  // это bubblesort
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                arr[j + 1] = temp; // это конец 
             }
         }
     }
