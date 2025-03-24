08946e32ba171e20f0ed118def3dc39129a292bc
nevertoomuch <_email@example.com>
2025-03-24 18:41:45 +0700

    Обновили sort.c

--- sort.c
+++ sort.c
@@ -1,11 +1,30 @@
-void bubbleSort(int arr[], int n) {  // это bubblesort
+#include <stdio.h>
+
+// Функция пузырьковой сортировки
+void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
+                // Меняем элементы местами
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
-                arr[j + 1] = temp; // это конец 
+                arr[j + 1] = temp;
             }
         }
     }
+}
+
+int main() {
+    int arr[] = {4, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
